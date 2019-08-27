38b00cd22d2abe5492048fc03d289869681d77fb
shabidkhan <54570265+shabidkhan@users.noreply.github.com>
2019-08-27 12:10:26 +0530

    Add files via upload

---
+++ debugging.py
@@ -0,0 +1,60 @@
+# print("we will leatn debugging by removin all the error from this python file by removin all the error from this python file") 
+
+
+# year = str(2016)
+# name = 'NavGurukul'
+# print(name+', '+year+" mein start hua tha!")
+
+
+# a=(int(input("enter the no.")))
+# b=(int(input("enter another no.")))
+# print(a+b,a-b,a*b,int(a/b),a%b)
+
+
+
+# price_milk = (int(input("1L milk ka price daalo?")))
+# print ("10L milk " + str(price_milk*10) + " rupees ka aata hai.") 
+
+
+
+# number =(float(input("please enter a decimal number")))
+# print ("your number divided by 2 is equal to = " + str(number/2))
+
+
+# mangoes=5
+# mangoes=mangoes+5
+# print(mangoes/5)
+
+
+
+# speed=(int(input("enter car's speed ")))
+# if (speed < 60 and speed>30):
+#     print ("Speed kam hai")
+# elif (speed < 30):
+#     print ("Speed bahot kam hai")
+# elif (speed > 100):
+#     print ("Speed bahot fast hai")
+
+
+
+# day = (str(input("Aaj ka din kya hai? (monday/tuesday) > ")))
+# time = (str(input("Kaunse samay ka khana banana hai? (lunch/dinner) > ")))
+# if (day == "monday" or day == "tuesday"):
+# 	if(time==lunch) :
+# 	 print ("Daal-Roti banegi aaj")
+#     elif (day == "tuesday" and time == "dinner"):
+#        print ("Pav-Bhaji banegi aaj toh :)")
+#     else:
+# 	   print("no info")
+# counter = 0 
+# while counter < 5:
+# 	print ("NavGurukul")
+# 	counter = counter + 1
+# print ("Yeh sirf ek baar print hoga")
+name="shivam"
+if name=="rishib":
+	print('rishib has given black jacket')
+	if name=='shivam':
+		print('shivam has given red jacket')
+else:
+	print('if somebody will not come so jacket is not given')		
